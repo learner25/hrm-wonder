2557d3890c277898126f516cf6359315a6e05483
learner25 <saimon2999@gmail.com>
2025-11-04 23:36:41 +0600

    promise fix

--- app/api/employees/[id]/route.ts
+++ app/api/employees/[id]/route.ts
@@ -1,25 +1,25 @@
 import { prisma } from "../../../lib/prisma";
 import { NextResponse } from "next/server";
 
-export async function GET(_req: Request, { params }: { params: { id: string } }) {
+export async function GET(_req: Request, { params }: { params: Promise<{ id: string }>}) {
   const emp = await prisma.employee.findUnique({
-    where: { id: Number(params.id) },
+    where: { id: Number((await params).id) },
     include: { department: true, attendance: true, leaves: true },
   });
   return NextResponse.json(emp);
 }
 
-export async function PUT(req: Request, { params }: { params: { id: string } }) {
+export async function PUT(req: Request, { params }: { params: Promise<{ id: string }> }) {
   const data = await req.json();
   const updated = await prisma.employee.update({
-    where: { id: Number(params.id) },
+    where: { id: Number((await params).id) },
     data,
   });
   return NextResponse.json(updated);
 }
 
 export async function PATCH(req: Request, { params }: { params: Promise<{ id: string }> }) {
-  const { id } = await params;  
+  const id = (await params).id;  
   const data = await req.json();
   const updated = await prisma.employee.update({
     where: { id: Number(id) },
@@ -30,7 +30,7 @@ export async function PATCH(req: Request, { params }: { params: Promise<{ id: st
 
 
 
-export async function DELETE(_req: Request, { params }: { params: { id: string } }) {
+export async function DELETE(_req: Request, { params }: { params: Promise<{ id: string }> }) {
   await prisma.employee.delete({ where: { id: Number(params.id) } });
   return NextResponse.json({ message: "Deleted successfully" });
 }
