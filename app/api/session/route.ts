1a44cda40370c42bd0e68401cd49fa9a3eb07579
learner25 <saimon2999@gmail.com>
2025-11-04 23:38:17 +0600

    delete a directory

--- app/api/session/route.ts
+++
@@ -1,7 +0,0 @@
-// // app/api/session/route.ts
-// import { getSession } from "@/app/lib/actions/auth-action";
-
-// export async function GET() {
-//   const session = await getSession();
-//   return Response.json({ session });
-// }
